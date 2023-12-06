93f83f579624b6c6fe077a035f59b5f765bf79bb
Brandie Lucano <blucano.dev@gmail.com>
2023-12-06 15:32:51 -0800

    set up complete

--- models.py
+++ models.py
@@ -1,1 +1,45 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """User of the site
+
+    Fields:
+        id: primary key
+        first_name: text, not null
+        last_name: text, not null
+        image_url: text, default=?
+    """
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    first_name = db.Column(
+        db.String(20),
+        nullable=False
+    )
+
+    last_name = db.Column(
+        db.String(20),
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.Text,
+        nullable=True
+    )
+
+
