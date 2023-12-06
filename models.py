cbefbeb5f59ee1618b43964751971d1217fe285b
Teagan McClintock <teagan.mcclintock@gmail.com>
2023-12-06 12:00:54 -0500

    Added models for tags and a join table between tags and posts. Changed app to import those models.

--- models.py
+++ models.py
@@ -90,5 +90,48 @@ class Post(db.Model):
     user = db.relationship('User', backref="posts")
 
 
+class Tag(db.Model):
+    """Tags about types of content
+    Fields:
+    id: primary key
+    name: String(20)
+    """
+
+    __tablename__ = 'tags'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    name = db.Column(
+        db.String(20),
+        nullable=False,
+        unique=True
+    )
+
+    posts = db.relationship('Post', secondary='posts_tags', backref='tags')
+
+class PostTag(db.Model):
+    """Connecting class between posts and tags
+    Fields:
+    post_id
+    tag_id
+    """
+
+    __tablename__ = 'posts_tags'
+
+    post_id = db.Column(
+        db.Integer,
+        db.ForeignKey('posts.id'),
+        primary_key=True
+    )
+
+    tag_id = db.Column(
+        db.Integer,
+        db.ForeignKey('tags.id'),
+        primary_key=True
+    )
 
 
