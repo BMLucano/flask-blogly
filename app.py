c1274965fcdfd261806612f5325c1ebe73eab06d
Brandie Lucano <blucano.dev@gmail.com>
2023-12-06 16:19:08 -0800

    created some routes

--- app.py
+++ app.py
@@ -17,3 +17,44 @@ connect_db(app)
 app.config['SECRET_KEY'] = "SECRET!"
 debug = DebugToolbarExtension(app)
 
+@app.get("/")
+def home_page():
+    """Redirects to list of users"""
+
+    return redirect("/users")
+
+@app.get("/users")
+def show_user_list():
+    """Show a list of all users and button to add a user"""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.get("/users/new")
+def show_create_user():
+    """Show form to create a new user."""
+
+    return render_template("create_user.html")
+
+@app.post('/users/new')
+def handle_create_user():
+    """Handle create user form and add new user to database.
+    Redirect to user list.
+    """
+
+    new_user = User(
+        first_name = request.form["first_name"],
+        last_name = request.form["last_name"],
+        image_url = request.form["image_url"]
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/users")
+
+@app.get("/users/<int:user_id>")
+def show_user_detail(user_id):
+    """Show details about a user with buttons to edit or delete profile."""
+    user = User.query.get(user_id)
+    return render_template("user_detail.html", user=user)
